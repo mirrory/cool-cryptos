f25cf11cbdefe13cf441220952dd625ced0a79e8
Melissa Iori <miori.code@gmail.com>
2022-11-16 21:55:19 -0500

    Switching to CoinGecko API

--- src/CryptoList.tsx
+++ src/CryptoList.tsx
@@ -1,25 +1,21 @@
 import './App.css';
 import {useEffect, useState} from 'react';
 
-interface CryptoAPIResponse {
-    data?: Crypto[]
-}
-
 interface Crypto {
     id?: string,
     name?: string
 }
 
 function CryptoList() {
-    const [state, setState] = useState({} as CryptoAPIResponse)
+    const [state, setState] = useState([] as Crypto[])
     useEffect(() => {
-        fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false", {mode: 'no-cors'}).then(
-            res => setState(res.json() as CryptoAPIResponse)
+        fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false", {}).then(
+            res => {res.json().then(data => setState(data))}
         )
     }, [])
     return (
         <>
-            {state["data"]?.map( d => <div>{d.name}</div>)} 
+            {state.map( d => <div>{d.name}</div>)} 
         </>
     )
 }
