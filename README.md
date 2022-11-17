beac7dab1009d2ab233ff2ea56904aa78b26bf3d
Melissa Iori <miori.code@gmail.com>
2022-11-16 23:24:26 -0500

    Add more data and improve the formatting

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # cool-cryptos
 A React app that shows the top 10 cryptocurrencies
+
+To run it from the command line:
+1. Have Node.js installed
+2. Clone this repository with `git clone`
+3. `cd cool-cryptos`
+4. `npm install`
+5. `npm start`
+6. Visit localhost:3000 in a browser
--- src/CryptoList.tsx
+++ src/CryptoList.tsx
@@ -1,58 +1,72 @@
 import './App.css';
-import {useEffect, useState, useRef} from 'react';
+import {useEffect, useState} from 'react';
 import Accordion from '@mui/material/Accordion';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import Typography from '@mui/material/Typography';
 
 interface Crypto {
+    current_price?: number,
     id?: string,
-    name?: string,
-    total_volume?: number,
-    symbol?: string,
     image?: string,
+    market_cap?: number,
+    market_cap_change_24h?: number,
+    market_cap_change_percentage_24h?: number
+    market_cap_rank?: number,
+    name?: string,
+    price_change_24h?: number,
     price_change_percentage_24h?: number,
-    market_cap_rank?: number
+    symbol?: string,
+    total_volume?: number
 }
 
 function CryptoList() {
-    let expandAccordionIcon = <svg xmlns="http://www.w3.org/2000/svg" width="16" height="18" viewBox="0 0 20 20" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="12" y1="5" x2="12" y2="19"></line><polyline points="19 12 12 19 5 12"></polyline></svg>;
+    const expandAccordionIcon = <svg xmlns="http://www.w3.org/2000/svg" width="16" height="18" viewBox="0 0 20 20" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="12" y1="5" x2="12" y2="19"></line><polyline points="19 12 12 19 5 12"></polyline></svg>;
     const [state, setState] = useState([] as Crypto[])
     const [initial, setInitial] = useState(true);
+    const [lastUpdate, setLastUpdate] = useState(new Date());
+
     useEffect(() => {
         async function getCoins(){
-            fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false", {}).then(
+            fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false", {})
+            .then(
                 res => {res.json().then(data => setState(data))}
             )
         }
         if (initial) {
-            getCoins()
+            getCoins();
             setInitial(false);   
         }
         const intervalId = setInterval(() => {
-            getCoins()
-          }, 1000 * 60) // every minute
+            getCoins();
+            setLastUpdate(new Date());
+          }, 1000 * 60) // Re-run the API fetch every minute
           return () => clearInterval(intervalId)
-    }, [state, initial])
+    }, [state, initial, lastUpdate])
+
     return (
         <>
             <h2 className='header'>Top 10 Cryptocurrencies by Market Cap</h2>
+            <h4 className='header'>Last Updated: {new Intl.DateTimeFormat("en-US", 
+            {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour:'numeric', minute: 'numeric'}).format(lastUpdate)}</h4>
             {state.map( coin => <Accordion className='coin'>
                 <AccordionSummary expandIcon={expandAccordionIcon}>
-                    <Typography>{coin.market_cap_rank}. <img className='coinImage' src={coin.image}></img>&nbsp;{coin.name} ({coin.symbol})</Typography>
+                    <Typography>{coin.market_cap_rank}. <img className='coinImage' alt={coin.name} src={coin.image}></img>&nbsp;{coin.name} ({coin.symbol})</Typography>
                 </AccordionSummary>
                 <AccordionDetails>
                     <Typography>
                         <table>
-                            <tr><td>Total Volume:</td><td>{coin.total_volume}</td></tr>
-                            <tr><td>Price Change Percentage (Last 24 Hours):</td>
-                            <td><span className={coin.price_change_percentage_24h!! < 0 ? 'negative' : 'positive'}>{coin.price_change_percentage_24h}%</span></td>
-                            </tr>
+                            <tr><td>Current Price:</td><td>{'$' + coin.current_price?.toLocaleString()}</td></tr>
+                            <tr><td>Price Change (Last 24 Hours):</td><td><span className={coin.price_change_24h!! < 0 ? 'negative' : 'positive'}>{'$' + coin.price_change_24h?.toLocaleString()}</span></td></tr>
+                            <tr><td>Price Change Percentage (Last 24 Hours):</td><td><span className={coin.price_change_percentage_24h!! < 0 ? 'negative' : 'positive'}>{coin.price_change_percentage_24h}%</span></td></tr>
+                            <tr><td>Total Volume:</td><td>{'$' + coin.total_volume?.toLocaleString()}</td></tr>
+                            <tr><td>Market Capitalization:</td><td>{'$' + coin.market_cap?.toLocaleString()}</td></tr>
+                            <tr><td>Market Cap Change (Last 24 Hours):</td><td><span className={coin.market_cap_change_24h!! < 0 ? 'negative' : 'positive'}>{'$' + coin.market_cap_change_24h?.toLocaleString()}</span></td></tr>
+                            <tr><td>Market Cap Change Percentage (Last 24 Hours):</td><td><span className={coin.market_cap_change_percentage_24h!! < 0 ? 'negative' : 'positive'}>{coin.market_cap_change_percentage_24h}%</span></td></tr>
                         </table>
                     </Typography>
                 </AccordionDetails>
-            </Accordion>)}
-            
+            </Accordion>)}         
         </>
     )
 }
